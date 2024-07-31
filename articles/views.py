6a9ebdf684fb52663a48f48b3e87f524980ca2f0
jjungcu519 <jjungcu0519@gmail.com>
2024-07-31 12:16:48 +0900

    create 기능을 함수 하나로 구현

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles' : articles,
+    }
+
+    return render(request, 'index.html', context)
+
+def create(request):
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+            
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+        else:
+            pass
+    else:
+        form = ArticleForm()
+
+        context = {
+            'form' : form,
+        }
+
+        return render(request, 'create.html', context)
